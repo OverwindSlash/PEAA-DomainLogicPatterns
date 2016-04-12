88df55b47033676db1f5a8e833ed338506e4f544
Dai Hao <overwind@outlook.com>
2016-04-12 23:36:48 +0800

    实现 Order Detail 的 Discount 计算

--- PEAA-DLP/TransactionScript/Service/NorthwindService.cs
+++ PEAA-DLP/TransactionScript/Service/NorthwindService.cs
@@ -10,13 +10,76 @@ namespace TransactionScript.Service
 {
     public class NorthwindService
     {
+        private const int DairyProductsCategory = 4;
+        private const int MeatCategory = 6;
+        private const int SeafoodCategory = 8;
+
         public void CalculateOrderDiscount(int orderId)
         {
             IList<OrderRelativeInfoDto> orderRelativeInfos = NorthwindTDG.FindOrderRelativeInfo(orderId).ToList();
 
             foreach (OrderRelativeInfoDto orderRelativeInfo in orderRelativeInfos)
             {
+                if (!orderRelativeInfo.CategoryID.HasValue)
+                {
+                    continue;
+                }
 
+                if (orderRelativeInfo.CategoryID.Value == DairyProductsCategory)
+                {
+                    if (orderRelativeInfo.Quantity < 10)
+                    {
+                        orderRelativeInfo.Discount = 0.00f;
+                    }
+                    else if (orderRelativeInfo.Quantity < 20)
+                    {
+                        orderRelativeInfo.Discount = 0.05f;
+                    }
+                    else if (orderRelativeInfo.Quantity < 30)
+                    {
+                        orderRelativeInfo.Discount = 0.10f;
+                    }
+                    else if (orderRelativeInfo.Quantity >= 30)
+                    {
+                        orderRelativeInfo.Discount = 0.15f;
+                    }
+                }
+
+                if (orderRelativeInfo.CategoryID.Value == MeatCategory)
+                {
+                    if (orderRelativeInfo.Quantity <= 10)
+                    {
+                        orderRelativeInfo.Discount = 0.00f;
+                    }
+                    else if (orderRelativeInfo.Quantity <= 20)
+                    {
+                        orderRelativeInfo.Discount = 0.05f;
+                    }
+                    else if (orderRelativeInfo.Quantity <= 40)
+                    {
+                        orderRelativeInfo.Discount = 0.10f;
+                    }
+                    else if (orderRelativeInfo.Quantity > 40)
+                    {
+                        orderRelativeInfo.Discount = 0.15f;
+                    }
+                }
+
+                if (orderRelativeInfo.CategoryID.Value == SeafoodCategory)
+                {
+                    if (orderRelativeInfo.Quantity <= 5)
+                    {
+                        orderRelativeInfo.Discount = 0.05f;
+                    }
+                    else if (orderRelativeInfo.Quantity <= 10)
+                    {
+                        orderRelativeInfo.Discount = 0.10f;
+                    }
+                    else if (orderRelativeInfo.Quantity > 10)
+                    {
+                        orderRelativeInfo.Discount = 0.15f;
+                    }
+                }
             }
         }
     }
